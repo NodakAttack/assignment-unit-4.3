2e834d743fe4ae61d21498977ce4a0e9120e10bf
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-06-22 20:44:48 -0500

    went well but broke something when modifying existing code during stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,80 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+const maxItems = 5;
+
+//add item to basket, return true
+function addItem (item){
+    if (isFull() === false){
+    basket.push(item);
+    return true;
+    }
+    else {
+        console.log(item, ' could not be added');
+        return false;
+    }
+}
+
+
+function listItems(){
+    for (let index = 0; index < basket.length; index++) {
+        console.log(basket[index]);
+    }
+}
+
+function empty(){
+    basket = [];
+}
+
+function isFull(){
+    if (basket.length < maxItems) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+ addItem('kale');
+ addItem('turnip');
+ addItem('shrimp');
+ addItem('panda');
+ addItem('carrot');
+
+//starting stretch goals
+
+
+
+// test if true or false
+
+console.log("-------------");
+
+
+empty();
+
+console.log("-------------");
+addItem("roast");
+listItems();
+
+/**
+ * 
+ * @param {*} item(string)
+ */
+
+function removeItem(item) {
+   console.log(basket.indexOf(item));
+}
+
+removeItem("roast");
+
+// testing code
+
+
+
+
+
+
+
 
 
 
